1670ddeb38e4cba8086442c6b37108a7b03c62a0
Jason Soto <jason@carbon.sia>
2021-02-10 16:59:15 -0500

    build with gtest

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+// Shared libs
 #include <iostream>
-
+// External libs
+#include "gtest/gtest.h"
+// My libs
 #include "node.hpp"
 
-int main(int, char**) {
+TEST(testing_gtest, assertion_check){
+    ASSERT_TRUE( 1 == 1 ); 
+}
+
+int main(int argc, char** argv) {
     std::cout << "Hello, world!\n";
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
